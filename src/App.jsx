b21f862f6c35f23c6cde4fdc30794b8fd7d12666
Serhii <mel.gipppson@gmail.com>
2022-12-04 19:13:34 +0200

    created start project structure, components and generel styles (index.css)

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Header } from './components/header/Header'
+import { About } from './components/about/About';
+import { Contact } from './components/contact/Contact';
+import { Experience } from './components/experience/Experience';
+import { Nav } from './components/nav/Nav';
+import { Footer } from './components/footer/Footer';
+
+export const App = () => {
+  return (
+    <>
+      <Header/>
+      <About/>
+      <Contact/>
+      <Experience/>
+      <Nav/>
+      <Footer/>
+    </>
+  )
+}
+
